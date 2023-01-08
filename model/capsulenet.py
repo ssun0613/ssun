e10e3a65260a95f7d52ca9c33abf487cf7c7d6f8
harimYi <kinglee214@gmail.com>
2023-01-08 20:20:44 +0900

    .

--- model/capsulenet.py
+++ model/capsulenet.py
@@ -19,6 +19,7 @@ class Primarycaps(nn.Module):
     def __init__(self, num_capsule=8, in_channels=256, out_channels=32, kernel_size=9, num_routes=32*6*6):
         super(Primarycaps, self).__init__()
         self.num_routes = num_routes
+        self.in_channels = in_channels
         self.capsules = nn.ModuleList([nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=2, padding=0) for _ in range(num_capsule)])
 
     def forward(self, x): # x.shape([1, 256, 44, 44])
@@ -33,7 +34,7 @@ class Primarycaps(nn.Module):
         return output_tensor
 
 class Digitcaps(nn.Module):
-    def __init__(self, num_capsules = 10, num_routes=32*6*6, in_channels=8, out_channels=16):
+    def __init__(self, num_capsules = 10, num_routes=32*6*6, in_channels=1, out_channels=72):
         super(Digitcaps, self).__init__()
 
         self.in_channels = in_channels
@@ -41,16 +42,16 @@ class Digitcaps(nn.Module):
         self.num_capsules = num_capsules
         self.num_routes = num_routes
 
-        self.W = nn.Parameter(torch.randn(1, num_routes, num_capsules, out_channels, in_channels))
+        self.W = nn.Parameter(torch.randn(1, num_routes, num_capsules, in_channels, out_channels))
 
     def forward(self, x): # x.shape([1, 1152, 72])
         batch_size = x.size(0) # batch_size=1
-        x = torch.stack([x]*self.num_capsules, dim=2).unsqueeze(3) # x.shape([1, 1152, 10, 72, 1])
+        x = torch.stack([x]*self.num_capsules, dim=2).unsqueeze(4) # x.shape([1, 1152, 10, 72, 1])
 
-        W = torch.cat([self.W]*batch_size, dim=0) # W.shape([1, 1152, 10, 16, 8])
-        u_hat = torch.matmul(W,x)
+        W = torch.cat([self.W]*batch_size, dim=0) # W.shape([1, 1152, 10, 1, 72])
+        u_hat = torch.matmul(W,x) # u_hat.shape([1, 1152, 10, 1, 1])
 
-        b_ij = Variable(torch.zeros(1, self.num_routes, self.num_capsules, 1))
+        b_ij = Variable(torch.zeros(1, self.num_routes, self.num_capsules, 1)) # b_ij.shape([1, 1152, 10, 1])
 
         num_iterations=3
         for iteration in range(num_iterations):
@@ -78,7 +79,7 @@ class Decoder(nn.Module):
         self.input_height = input_height
         self.input_channel = input_channel
         self.reconstraction_layers = nn.Sequential(
-            nn.Linear(16*10, 512),
+            nn.Linear(10, 512),
             nn.ReLU(),
             nn.Linear(512, 1024),
             nn.ReLU(),
@@ -93,7 +94,7 @@ class Decoder(nn.Module):
         _, max_length_indices = classes.max(dim=1)
         masked = Variable(torch.sparse.torch.eye(10))
 
-        masked = masked.index_selext(dim=0, index=Variable(max_length_indices.squeeze(1).data))
+        masked = masked.index_select(dim=0, index=Variable(max_length_indices.squeeze(1).data))
         t = (x*masked[:,:,None,None]).view(x.size(0),-1)
         reconstructions = self.reconstraction_layers(t)
         reconstructions = reconstructions.view(-1, self.input_channel, self.input_width, self.input_height)
@@ -104,8 +105,10 @@ class capsnet(nn.Module):
         super(capsnet, self).__init__()
         self.conv_layer = Conv(in_channels=1, out_channels=256, kernel_size=9)
         self.primary_layer = Primarycaps(num_capsule=8, in_channels=256, out_channels=32, kernel_size=9, num_routes=32*6*6)
-        self.digit_capsules = Digitcaps(num_capsules = 10, num_routes=32*6*6, in_channels=8, out_channels=16)
+        self.digit_capsules = Digitcaps(num_capsules = 10, num_routes=32*6*6, in_channels=1, out_channels=72)
         self.decoder = Decoder(input_width=28, input_height=28, input_channel=1)
+
+
 
         self.mse_loss = nn.MSELoss()
 
